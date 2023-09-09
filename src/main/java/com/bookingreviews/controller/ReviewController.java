c6c5717d23920b931e3a7af04c753d8b632892f0
markuza99 <67234203+markuza99@users.noreply.github.com>
2023-09-09 13:27:30 +0200

    feat: change api route (#4)
    * feat: change api route
    
    * bugfix: move string to proper annotation

--- src/main/java/com/bookingreviews/controller/ReviewController.java
+++ src/main/java/com/bookingreviews/controller/ReviewController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
 
